ede90446b4d67d5fb143c6ba7fe4bb78f5de9b40
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2023-04-16 13:11:23 +0000

    Object-orientation Person

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,16 @@
+struct Person {
+    name: String,
+    age: i8,
+    hobby: String,
+}
+
+impl Person {
+    fn new(name: String, age: i8, hobby: String) -> Self {
+        Person { name, age, hobby }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bob = Person::new("ボブ".to_string(), 23, "ヒップホップ".to_string());
+    println!("{}は{}歳で趣味は{}です。", bob.name, bob.age, bob.hobby);
 }
